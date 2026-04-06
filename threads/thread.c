121b891a63c74437698ad4f31bfc1920ebfaa963
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-04-06 21:28:14 +0900

    fix: update `thread_set_priority` to conditionally adjust `effective_priority`
    - Ensure `effective_priority` is updated only if the new priority is higher.

--- threads/thread.c
+++ threads/thread.c
@@ -360,7 +360,8 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
-  thread_current ()->effective_priority = new_priority;
+  if (thread_current()->effective_priority < new_priority)
+    thread_current ()->effective_priority = new_priority;
 
   if (intr_context()) {
     intr_yield_on_return();
