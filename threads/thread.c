9364de23a69e9bf2598bde76b1925f0c7ecec48c
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-04-06 12:44:29 +0900

    fix: remove debug logs for priority and semaphore checks in `thread.c`

--- threads/thread.c
+++ threads/thread.c
@@ -265,9 +265,7 @@ thread_unblock (struct thread *t)
 
   struct thread *cur = thread_current ();
 
-  printf("t waiting on sema?: %s\n", t->waiting_on_sema ? "true" : "false");
   if (cur != idle_thread && (t->waiting_on_sema || t->priority > cur->priority)) {
-    printf("t priority: %d, current t priority: %d\n", t->priority, cur->priority);
     if (intr_context()) {
       intr_yield_on_return();
     } else {
