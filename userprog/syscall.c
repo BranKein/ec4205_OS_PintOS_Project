47892cb025798907f2c3e20dd0c7e0011bb0c5cc
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-05-12 21:41:30 +0900

    refactor: modularize syscall handling by introducing individual syscall functions
    - Extract specific system calls (e.g., `sys_exit`, `sys_read`) into dedicated functions.
    - Update `syscall_handler()` to delegate handling to these modular functions for improved readability and maintainability.

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -6,6 +6,19 @@
 
 static void syscall_handler (struct intr_frame *);
 
+void sys_halt (struct intr_frame *);
+void sys_exit (struct intr_frame *);
+void sys_wait (struct intr_frame *);
+void sys_create (struct intr_frame *);
+void sys_remove (struct intr_frame *);
+void sys_open (struct intr_frame *);
+void sys_filesize (struct intr_frame *);
+void sys_read (struct intr_frame *);
+void sys_write (struct intr_frame *);
+void sys_seek (struct intr_frame *);
+void sys_tell (struct intr_frame *);
+void sys_close (struct intr_frame *);
+
 void
 syscall_init (void) 
 {
@@ -20,35 +33,71 @@ syscall_handler (struct intr_frame *f)
   int syscall_num = *(int*)(f->esp);
   switch (syscall_num) {
     case SYS_HALT:
+      sys_halt (f);
       break;
-    case SYS_EXIT: {
-      int exit_code = *(int*)(f->esp + 4);
-      thread_current()->exit_code = exit_code;
-      thread_exit ();
+    case SYS_EXIT:
+      sys_exit (f);
       break;
-    }
     case SYS_WAIT:
+      sys_wait (f);
       break;
     case SYS_CREATE:
+      sys_create (f);
       break;
     case SYS_REMOVE:
+      sys_remove (f);
       break;
     case SYS_OPEN:
+      sys_open (f);
       break;
     case SYS_FILESIZE:
+      sys_filesize (f);
       break;
     case SYS_READ:
+      sys_read (f);
       break;
     case SYS_WRITE:
+      sys_write (f);
       break;
     case SYS_SEEK:
+      sys_seek (f);
       break;
     case SYS_TELL:
+      sys_tell (f);
       break;
     case SYS_CLOSE:
+      sys_close (f);
       break;
     default:
       printf ("unknown system call %d\n", syscall_num);
       break;
   }
 }
+
+void sys_halt (struct intr_frame *f) {}
+
+void sys_exit (struct intr_frame *f) {
+  int exit_code = *(int*)(f->esp + 4);
+  thread_current()->exit_code = exit_code;
+  thread_exit ();
+}
+
+void sys_wait (struct intr_frame *f) {}
+
+void sys_create (struct intr_frame *f) {}
+
+void sys_remove (struct intr_frame *f) {}
+
+void sys_open (struct intr_frame *f) {}
+
+void sys_filesize (struct intr_frame *f) {}
+
+void sys_read (struct intr_frame *f) {}
+
+void sys_write (struct intr_frame *f) {}
+
+void sys_seek (struct intr_frame *f) {}
+
+void sys_tell (struct intr_frame *f) {}
+
+void sys_close (struct intr_frame *f) {}
