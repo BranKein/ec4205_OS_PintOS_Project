dbfec43aa580542e3b7ad5a6a63672aa6a544ead
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-05-13 00:23:23 +0900

    feat: implement additional syscalls and fix `sys_halt`
    - Complete implementations for `sys_halt`, `sys_exit`, `sys_exec`, `sys_wait`, `sys_create`, and `sys_remove`.
    - Partially implement `sys_read`, `sys_open`, `sys_filesize`, `sys_seek`, `sys_tell`, and `sys_close`.
    - Add support for shutdown in `sys_halt` and process loading in `sys_exec`.
    - Update `sys_exit` to correctly retrieve and set exit status.

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,13 +1,16 @@
 #include "userprog/syscall.h"
+#include "userprog/process.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
 void sys_halt (struct intr_frame *);
 void sys_exit (struct intr_frame *);
+void sys_exec (struct intr_frame *);
 void sys_wait (struct intr_frame *);
 void sys_create (struct intr_frame *);
 void sys_remove (struct intr_frame *);
@@ -37,6 +40,9 @@ syscall_handler (struct intr_frame *f)
       break;
     case SYS_EXIT:
       sys_exit (f);
+      break;
+    case SYS_EXEC:
+      sys_exec (f);
       break;
     case SYS_WAIT:
       sys_wait (f);
@@ -74,25 +80,66 @@ syscall_handler (struct intr_frame *f)
   }
 }
 
-void sys_halt (struct intr_frame *f) {}
+void sys_halt (struct intr_frame *f) {
+  shutdown_power_off();
+}
 
 void sys_exit (struct intr_frame *f) {
-  int exit_code = *(int*)(f->esp + 4);
-  thread_current()->exit_code = exit_code;
+  int status = *(int*)(f->esp + 4);
+  thread_current()->exit_code = status;
   thread_exit ();
 }
 
-void sys_wait (struct intr_frame *f) {}
+void sys_exec (struct intr_frame *f) {
+  const char *cmd_line = *(char**)(f->esp + 4);
+  tid_t pid = process_exec (cmd_line);
+  f->eax = pid;
 
-void sys_create (struct intr_frame *f) {}
+  // TODO: the parent process cannot return from the exec until it knows whether the child process successfully loaded its executable. You must use appropriate synchronization to ensure this.
+}
 
-void sys_remove (struct intr_frame *f) {}
+void sys_wait (struct intr_frame *f) {
+  pid_t pid = *(pid_t*)(f->esp + 4);
+  int status = process_wait (pid);
+  f->eax = status;
 
-void sys_open (struct intr_frame *f) {}
+  // TODO: handling exception without exit call?
+}
 
-void sys_filesize (struct intr_frame *f) {}
+void sys_create (struct intr_frame *f) {
+  const char *file = *(char**)(f->esp + 4);
+  unsigned initial_size = *(unsigned*)(f->esp + 8);
+  f->eax = filesys_create (file, initial_size);
+}
 
-void sys_read (struct intr_frame *f) {}
+void sys_remove (struct intr_frame *f) {
+  const char *file = *(char**)(f->esp + 4);
+  f->eax = filesys_remove (file);
+}
+
+void sys_open (struct intr_frame *f) {
+  const char *file = *(char**)(f->esp + 4);
+  // TODO: return type mismatch
+  // f->eax = filesys_open (file);
+}
+
+void sys_filesize (struct intr_frame *f) {
+  int fd = *(int*)(f->esp + 4);
+  // TODO: check!
+  // f->eax = filesys_filesize (fd);
+}
+
+void sys_read (struct intr_frame *f) {
+  int fd = *(int*)(f->esp + 4);
+  const void *buffer = *(void **)(f->esp + 8);
+  unsigned size = *(unsigned*)(f->esp + 12);
+  if (fd == 0) {
+    input_getc(buffer, size);
+    f->eax = size;
+  } else {
+    f->eax = -1;
+  }
+}
 
 void sys_write (struct intr_frame *f) {
   int fd = *(int*)(f->esp + 4);
@@ -106,8 +153,20 @@ void sys_write (struct intr_frame *f) {
   }
 }
 
-void sys_seek (struct intr_frame *f) {}
+void sys_seek (struct intr_frame *f) {
+  int fd = *(int*)(f->esp + 4);
+  unsigned position = *(unsigned*)(f->esp + 8);
+  // TODO: ??
+}
 
-void sys_tell (struct intr_frame *f) {}
+void sys_tell (struct intr_frame *f) {
+  int fd = *(int*)(f->esp + 4);
+  // TODO: check!
+  // filesys_tell(fd);
+}
 
-void sys_close (struct intr_frame *f) {}
+void sys_close (struct intr_frame *f) {
+  int fd = *(int*)(f->esp + 4);
+  // TODO: check!
+  // filesys_close(fd);
+}
