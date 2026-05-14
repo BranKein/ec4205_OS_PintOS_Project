7b3491d41da1959f47a29a890c1f843f5235692b
YeonhyukKim <brankein13@gm.gist.ac.kr>
2026-05-14 21:44:16 +0900

    include: update file include in `thread.h` to `filesys/file.h`

--- threads/thread.h
+++ threads/thread.h
@@ -5,7 +5,7 @@
 #include <list.h>
 #include <stdint.h>
 #include "threads/synch.h"
-#include "userprog/file.h"
+#include "filesys/file.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
